5d13237c40ff7160bfe16496db19ac421ac3f982
admin@aws-service.internal <you@example.com>
2020-12-19 00:30:45 +0000

    fix bugs in the script; improve consistency

--- polling-ci/custom_ci.sh
+++ polling-ci/custom_ci.sh
@@ -5,12 +5,11 @@ while true; do
     if [ ! -d "./prysm" ]; then
         git clone --branch "$GIT_BRANCH" --recurse-submodules --depth 1 https://github.com/prysmaticlabs/prysm
     fi
-    cd prysm
 
     # checking for updates
-    git fetch
-    LOCAL=$(git rev-parse develop);
-    REMOTE=$(git rev-parse origin/develop);
+    (cd prysm && git fetch)
+    LOCAL=$(cd prysm && git rev-parse develop);
+    REMOTE=$(cd prysm && git rev-parse origin/develop);
     # echo -e \#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\#
     # echo $LOCAL
     # echo ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -20,11 +19,9 @@ while true; do
     if [ $LOCAL != $REMOTE ]; then
 	shortName=$REMOTE:0:10
 	echo -e new commit "$shortName" - pulling/running...
-        git pull -f develop;
+	(cd prysm && git pull -f develop)
 	# passing arguments to the runner:
         ./run_eth2fuzz.sh -f -n commit-$shortName -t 600
     fi
-
-    cd ..
 sleep 10
 done
